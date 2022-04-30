da556bcc4241a9e3034f254a89d8e47f1bd2763f
blendF <blendi@Blendi.localdomain>
2022-04-30 02:46:56 +0200

    initial commiting

---
+++ testcode.js
@@ -0,0 +1,14 @@
+function module123() {
+
+    var inputa = window.prompt("Insert your name!");
+
+    if (inputa == "blendF") {
+     alert("This is yor github repository: " +
+      "https://github.com/blendF/firstreposDemo.git");
+    }
+    else
+    {
+        alert("user unknown !");
+    }
+
+}
